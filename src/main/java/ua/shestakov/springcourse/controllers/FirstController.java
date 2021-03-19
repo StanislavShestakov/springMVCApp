1c2d76dea5ba4773ffb922ac228340ea6130055f
admin <w3orks@mail.ru>
2021-03-19 14:40:05 +0300

    spring GET by HttpServletRequest

--- src/main/java/ua/shestakov/springcourse/controllers/FirstController.java
+++ src/main/java/ua/shestakov/springcourse/controllers/FirstController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest requet){
+            String name= requet.getParameter("name");
+            String surname= requet.getParameter("surname");
+
+
+        System.out.println("Hello, "+name + " " + surname) ;
             return "first/hello";
     }
     @GetMapping("/goodbye")
